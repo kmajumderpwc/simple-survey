bdf308f8701f6469bf8fbd3f10b8d118a65da0ae
kmajumderpwc <43469539+kmajumderpwc@users.noreply.github.com>
2018-11-12 20:05:00 +0530

    Update score.js

--- score.js
+++ score.js
@@ -4,6 +4,15 @@ var bodyParser = require('body-parser');
 var fs = require("fs");
 app.use(bodyParser.json())
 var path = require ('path');
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "kaustav"
+});
+
 
 
 app.get('/getScore', function (req, res) {
@@ -39,7 +48,39 @@ app.get('/lastPage', function(req,res){
      res.send();
   });
 })
+
+app.post('/saveData',function(req,res){
+  //console.log("req is ::",req)
+  var username = req.query.username;
+  var email = req.query.email;
+  var organization = req.query.organization;
+  var mobile = req.query.mobile;
+  console.log("username is ",username);
+  console.log("email is ",email);
+  console.log("organization is ",organization);
+  console.log("mobile is ",mobile);
+
+  con.connect(function(err){
+    var insertSql = "INSERT into employee (username,email,organization,mobile) VALUES(?,?,?,?) ";
+     con.query(insertSql ,[username,email,organization,mobile], function(err,rows){
+         if (!err) {  
+             res.json(rows);  
+         }  
+         else {  
+             res.json(err);  
+         }  
+        
+     })
+ });
+
+  res.send();
+});
+
 app.use(express.static(path.join(__dirname,'public')));
+app.use(bodyParser.text({type:['text/xml', 'application/xml', 'text/html']}));
+app.use(bodyParser.json({type:['application/json']}));
+
+
 app.listen(8080, () => {
   console.log('App running..')
   console.log('Visit localhost:8080/getScore')
